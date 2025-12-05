bdfca54d8de212a10fb35d777be5d1f371144e43
Steven Huynh <163560160+duy1066@users.noreply.github.com>
2025-12-05 11:00:39 -0800

    Update commands.ps1

--- 01-domain-controller-and-user-administration/commands.ps1
+++ 01-domain-controller-and-user-administration/commands.ps1
@@ -1,1 +1,25 @@
 
+Paste this:
+
+```powershell
+# Rename Server
+Rename-Computer -NewName "DC01" -Restart
+
+# Install AD DS Role
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+# Promote to Domain Controller
+Install-ADDSForest -DomainName "stevenlab.local"
+
+# Create OUs
+New-ADOrganizationalUnit -Name "Users" -Path "DC=stevenlab,DC=local"
+New-ADOrganizationalUnit -Name "Admins" -Path "DC=stevenlab,DC=local"
+New-ADOrganizationalUnit -Name "Computers" -Path "DC=stevenlab,DC=local"
+
+# Create Standard User
+New-ADUser -Name "John Doe" -SamAccountName jdoe -AccountPassword (Read-Host -AsSecureString) -Enabled $true
+
+# Create Groups
+New-ADGroup -Name "Standard Users" -GroupScope Global -Path "OU=Users,DC=stevenlab,DC=local"
+New-ADGroup -Name "IT Admins" -GroupScope Global -Path "OU=Admins,DC=stevenlab,DC=local"
+
